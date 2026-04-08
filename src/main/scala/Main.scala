175173791969da06ccacb41a780cde796d4ea36b
Franco Galassi <fragalassi@mi.unc.edu.ar>
2026-04-08 20:30:58 -0300

    fix: main con soporte para type Subscription

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,16 @@
 object Main {
+
+  type Subscription = (String, String)
+
   def main(args: Array[String]): Unit = {
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
-    val subscriptions: List[String] = FileIO.readSubscriptions()
+    val subscriptions: List[Subscription] = FileIO.readSubscriptions()
 
-    val allPosts: List[(String, String)] = subscriptions.map { url =>
+    val allPosts: List[(String, String)] = subscriptions.map { case (name, url) =>
       println(s"Fetching posts from: $url")
       val posts = FileIO.downloadFeed(url)
-      (url, posts)
+      (name, posts)
     }
 
     val output = allPosts
