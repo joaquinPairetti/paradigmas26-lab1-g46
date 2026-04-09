3ece2188afe2c7a401a8fad88019f73dd8010520
Franco Galassi <fragalassi@mi.unc.edu.ar>
2026-04-09 19:33:28 -0300

    feat: formato básico inicial para titulos de posts

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -1,8 +1,11 @@
 
 object Formatters {
 
+  type Post = (String, String, String, String)
+
+
   // Pure function to format posts from a subscription
-  def formatSubscription(url: String, posts: String): String = {
+  def formatSubscription(url: String, posts: List[Post]): String = {
     val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
     val formattedPosts = posts.take(80)
     header + "\n" + formattedPosts
