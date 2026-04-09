ce072072a0cd9b696f7b0dad67e6bf5bfdb6df4e
Franco Galassi <fragalassi@mi.unc.edu.ar>
2026-04-09 19:31:20 -0300

    fix: cambios en tipo post y argumentos de funciones

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,20 +1,21 @@
 object Main {
 
   type Subscription = (String, String)
+  type Post = (String, String, String, String)
 
   def main(args: Array[String]): Unit = {
     val header = s"Reddit Post Parser\n${"=" * 40}"
 
     val subscriptions: List[Subscription] = FileIO.readSubscriptions()
 
-    val allPosts: List[(String, String)] = subscriptions.map { case (name, url) =>
+    val allPosts: List[(String, List[Post])] = subscriptions.map { case (name, url) =>
       println(s"Fetching posts from: $url")
-      val posts = FileIO.downloadFeed(url)
+      val posts = FileIO.downloadJson(url)
       (name, posts)
     }
 
     val output = allPosts
-      .map { case (url, posts) => Formatters.formatSubscription(url, posts) }
+      .map { case (name, posts) => Formatters.formatSubscription(name, posts) }
       .mkString("\n")
 
     println(output)
