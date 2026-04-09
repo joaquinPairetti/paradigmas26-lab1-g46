5019d371b1fc98321f2178b669dd858d2823badd
Franco Galassi <fragalassi@mi.unc.edu.ar>
2026-04-09 20:14:12 -0300

    fix: el formato no fue agregado antes, ahora si

--- src/main/scala/Formatters.scala
+++ src/main/scala/Formatters.scala
@@ -7,7 +7,13 @@ object Formatters {
   // Pure function to format posts from a subscription
   def formatSubscription(url: String, posts: List[Post]): String = {
     val header = s"\n${"=" * 80}\nPosts from: $url \n${"=" * 80}"
-    val formattedPosts = posts.take(80)
-    header + "\n" + formattedPosts
+
+    val formattedPosts = posts.map {case (subreddit, title, selftext, date) =>{
+    s"\n${"-" * 160} \nr/$subreddit - $date \n   $title  \n${"-" * 160} \n \n $selftext"
+    }.mkString
+
+    header + formattedPosts
+
   }
+}
 }
